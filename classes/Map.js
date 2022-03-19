a2c040a5b8fb64206e9e989987a141a96c89641d
leemch <lee7mchale@gmail.com>
2022-03-19 15:17:16 -0700

    added map classes

--- classes/Map.js
+++ classes/Map.js
@@ -1,7 +1,27 @@
+import Tile from "./Tile";
+
+
 class Map {
-    constructor(name, data) {
+    constructor(name, height, width) {
         this.name = name;
-        this.data = data;
+        this.height = height;
+        this.width = width;
+        this.tiles = new Array(height).fill(0).map(row => new Array(width).fill(0));
+        for(let x = 0; x < this.tiles.length; x++) {
+            for(let y = 0; y < this.tiles[0].length; y++) {
+                this.tiles[x][y] = new Tile();
+            }
+        }
+        console.log(this.tiles);
+    }
+
+
+    getTile(x, y) {
+        if(x < this.height && x >= 0 && y < this.width && y >= 0) {
+            return this.tiles[y][x];
+        }
+
+        return null;
     }
 }
 
--- classes/Tile.js
+++ classes/Tile.js
@@ -1,7 +1,34 @@
 class Tile {
-    constructor(sprite_id) {
-        this.sprite_id = sprite_id;
+    constructor() {
         this.data = [];
+    }
+
+    getData() {
+        return this.data;
+    }
+
+    getTopObject() {
+        const tileData = this.getData();
+        if(tileData.length > 0) {
+            return tileData[0];
+        }
+
+        return null;
+    }
+
+    addObject(obj) {
+        const tileData = this.getData();
+
+        tileData.push(obj);
+    }
+
+    removeFromTop() {
+        const tileData = this.getData();
+        if(tileData.length > 0) {
+            return tileData.pop();
+        }
+
+        return null;
     }
 }
 
